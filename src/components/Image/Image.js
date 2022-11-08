c04bdf0dd5e46cd767db922fc977ac8e648f281a
sasban <azure.saswata@gmail.com>
2022-11-08 15:59:05 +0530

    383 updating posts

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -6,7 +6,7 @@ const image = props => (
   <div
     className="image"
     style={{
-      backgroundImage: `url('${props.imageUrl}')`,
+      backgroundImage: `url(${props.imageUrl})`,
       backgroundSize: props.contain ? 'contain' : 'cover',
       backgroundPosition: props.left ? 'left' : 'center'
     }}
