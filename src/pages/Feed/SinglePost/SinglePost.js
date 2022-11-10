f39ad6929451777f05bad670323d72b766b8c52a
sasban <azure.saswata@gmail.com>
2022-11-10 20:15:34 +0530

    385 Adding pagination

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -25,7 +25,7 @@ class SinglePost extends Component {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
-          image: 'http://localhost:8080/' + resData.post.imageUrl,
+          image: 'http://localhost:8080/' + (resData.post.imageUrl).split('\\').join('\\\\'),
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
         });
