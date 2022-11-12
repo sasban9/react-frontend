55f680fa56f5b407b585a617d8a3cdf6ee1ea264
sasban <azure.saswata@gmail.com>
2022-11-12 16:57:30 +0530

    412 Sharing the IO instance across files

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -40,7 +40,12 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket("http://localhost:8080");
+    const socket = openSocket("http://localhost:8080");
+    socket.on('posts', data => {
+      if(data.action === 'create') {
+        this.addPost(data.post);
+      }
+    })
   }
 
   addPost = (post) => {
