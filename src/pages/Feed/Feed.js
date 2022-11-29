d448b36dcce92d652521fa203846f7bdff3751a5
sasban <azure.saswata@gmail.com>
2022-11-30 00:11:01 +0530

    435 Sending "Create Post" and "Get Post" Queries

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -93,26 +93,39 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch("http://localhost:8080/feed/posts?page=" + page + "&perPage=" + this.state.perPage, {
+    const graphqlQuery = {
+      query: `
+        {
+          posts {
+            posts {title content _id creator{name} createdAt}
+            totalPosts
+          }
+        }
+      `
+    };
+    fetch("http://localhost:8080/graphql", {
+      method: "POST",
       headers: {
         Authorization: "Bearer " + this.props.token,
-      }
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(graphqlQuery)
     })
       .then((res) => {
-        if (res.status !== 200) {
-          throw new Error("Failed to fetch posts.");
-        }
         return res.json();
       })
       .then((resData) => {
+        if(resData.errors) {
+          throw new Error("Fetching Posts failed!");
+        }
         this.setState({
-          posts: resData.posts.map((post) => {
+          posts: resData.data.posts.posts.map((post) => {
             return {
               ...post,
               imagePath: post.imageUrl,
             };
           }),
-          totalPosts: resData.totalItems,
+          totalPosts: resData.data.posts.totalPosts,
           perPage: resData.perPage,
           postsLoading: false,
         });
@@ -216,16 +229,16 @@ class Feed extends Component {
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
+          } else {
+            updatedPosts.unshift(post);
           }
           return {
-            // posts: updatedPosts,
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
           };
-        });
+        });        
       })
       .catch((err) => {
         console.log(err);
