f1ee5c774ead28e5d275fb3136c71f47758349d7
sasban <azure.saswata@gmail.com>
2022-12-01 16:06:41 +0530

    439 Viewing a Single Post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -237,13 +237,15 @@ class Feed extends Component {
         };
         this.setState((prevState) => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
-            updatedPosts.pop();
+            updatedTotalPosts++;
+            if(prevState.posts.length >= 2) updatedPosts.pop();
             updatedPosts.unshift(post);
           }
           return {
@@ -251,6 +253,7 @@ class Feed extends Component {
             isEditing: false,
             editPost: null,
             editLoading: false,
+            totalPosts: updatedTotalPosts
           };
         });        
       })
